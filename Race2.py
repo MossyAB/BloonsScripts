8b8eb9e3899e510fddfe0fbc4a9fe9d23870db5c
Alfie <44749405+MossyAB@users.noreply.github.com>
2023-09-28 16:06:36 +0100

    Add files via upload

---
+++ Race2.py
@@ -0,0 +1,33 @@
+import time
+import pyautogui
+
+
+time.sleep(5)
+while True:
+    pyautogui.moveTo(1820, 348)
+    pyautogui.click(button='left')
+    time.sleep(0.25)
+
+    pyautogui.moveTo(840, 855)
+    pyautogui.click(button='left')
+    time.sleep(3)
+
+    pyautogui.moveTo(961, 761)
+    pyautogui.click(button='left')
+    time.sleep(0.25)
+
+    pyautogui.moveTo(1835, 1009)
+    pyautogui.click(button='left')
+    time.sleep(0.25)
+
+    pyautogui.moveTo(1593, 61)
+    pyautogui.click(button='left')
+    time.sleep(0.25)
+
+    pyautogui.moveTo(830, 845)
+    pyautogui.click(button='left')
+    time.sleep(0.25)
+
+    time.sleep(3)
+
+
